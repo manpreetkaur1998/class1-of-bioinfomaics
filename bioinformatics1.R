9eea4697e76aa738a53d07dba30d38b39c2b1685
unknown <mkaur2563@gmail.com>
2021-05-12 22:11:29 +0530

    class1

---
+++ bioinformatics1.R
@@ -0,0 +1,52 @@
+#some arithmetic function
+# addition
+3+6
+1+5
+46+98
+# substraction
+7-4
+19-16
+65-43
+7-9
+# square root
+sqrt(16)
+sqrt(78)
+# exponents
+13^6
+13^2
+2^3
+23^2
+# multiplication
+8*2
+4*5
+# division
+100/50
+# mean
+mean(1,10,100) #result should be 37
+mean.default(1,10,100) #result should be 10
+median(1,10,100)
+?median
+?mean
+(c(1,10,100)) #to store our series of values
+mean(c(1,10,100))
+(c(20,40,80))
+mean(c(20,40,80))
+median(c(20,40,80))
+mode(c(20,40,80))
+# to store the series of number in a variable = <-
+a=(c(12,14,15))
+a<- (c(45,67,89))
+a
+mean(a)
+median(a)
+a
+mean(a)
+a=(c(34,45,67))
+a
+a<-(c(10,67,89))
+a
+mean(a)
+max(a)
+min(a)
+4+5
+15+56
